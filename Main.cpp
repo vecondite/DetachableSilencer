5d37b36e19732ea977b72e6416ce89782cbb563e
vecondite <vecondite@gmail.com>
2026-09-21 21:43:15 +0530

    Extra features and Refactor Main.cpp for improved configuration handling
    - added a custom animation
    - added an actual silencer model to player's hands

--- Main.cpp
+++ Main.cpp
@@ -1,118 +1,276 @@
 #include <plugin.h> // Plugin-SDK version 1005 from 2026-08-14 08:10:00
-#include <CStreaming.h>
-#include <CWeaponInfo.h>
-#include <extensions/ScriptCommands.h>
+#include <CMessages.h>
 #include <mini/ini.h>
-#include <CCheat.h>
 #include <CHud.h>
+#include <CStreaming.h>
+#include <extensions/ScriptCommands.h>
+#include <CWorld.h>
+#include <CCheat.h>
 
 using namespace plugin;
 
+unsigned int keyBind = 255;
+std::string cheatCode;
+unsigned int boneId;
+float xOffset;
+float yOffset;
+float zOffset;
+bool silencer;
+std::string ifpName;
+std::string animName;
+unsigned int animDuration;
+unsigned int animSpot;
+unsigned int silencerModel;
+
+mINI::INIFile file("config.ini");
+mINI::INIStructure ini;
+
+void readConfig() {
+    keyBind = 192;
+    cheatCode = "DSP";
+    boneId = 35;
+    xOffset = 0.03;
+    yOffset = 0.0;
+    zOffset = -0.01;
+    ifpName = "silencer";
+    animName = "silencer";
+    animDuration = 1167;
+    animSpot = 900;
+    silencerModel = 328;
+    silencer = 0;
+
+    static char msg[1024];
+
+    if (!file.read(ini)) {
+        ini["settings"]["keybind"] = std::to_string(keyBind);
+        ini["settings"]["cheatcode"] = cheatCode;
+        ini["settings"]["boneid"] = std::to_string(boneId);
+        ini["settings"]["xoffset"] = std::to_string(xOffset);
+        ini["settings"]["yoffset"] = std::to_string(yOffset);
+        ini["settings"]["zoffset"] = std::to_string(zOffset);
+        ini["settings"]["ifp"] = ifpName;
+        ini["settings"]["anim"] = animName;
+        ini["settings"]["animduration"] = std::to_string(animDuration);
+        ini["settings"]["animspot"] = std::to_string(animSpot);
+        ini["settings"]["silencerModel"] = std::to_string(silencerModel);
+        ini["saves"]["silencer"] = std::to_string(silencer);
+        file.generate(ini);
+
+        sprintf_s(msg, "Config ~r~not found!~w~~n~~n~"
+                        "~b~Default config~w~ with values:~n~~n~"
+                        "Keybind: ~b~%d~w~~n~"
+                        "Cheat code: ~b~%s~w~~n~"
+                        "BoneID: ~b~%d~w~~n~"
+                        "xyzOffset: ~b~%f %f %f~w~~n~"
+                        "ifp: ~b~%s~w~~n~"
+                        "anim: ~b~%s~w~~n~"
+                        "AD: ~b~%d~w~~n~"
+                        "AS: ~b~%d~w~~n~"
+                        "M: ~b~%d~w~~n~"
+                        "Silencer: ~b~%d~w~",
+            keyBind, cheatCode.c_str(), boneId, xOffset, yOffset, zOffset, ifpName.c_str(), animName.c_str(), animDuration, animSpot, silencerModel, silencer);
+        CHud::SetHelpMessage(msg, true, false, false);
+    }
+    try {
+        file.read(ini);
+        keyBind = std::stoul(ini["settings"]["keyBind"], nullptr, 0);
+        cheatCode = ini["settings"]["cheatcode"];
+        boneId = std::stoi(ini["settings"]["boneid"]);
+        xOffset = std::stof(ini["settings"]["xoffset"]);
+        yOffset = std::stof(ini["settings"]["yoffset"]);
+        zOffset = std::stof(ini["settings"]["zoffset"]);
+        ifpName = ini["settings"]["ifp"];
+        animName = ini["settings"]["anim"];
+        animDuration = std::stoi(ini["settings"]["animduration"]);
+        animSpot = std::stoi(ini["settings"]["animspot"]);
+        silencerModel = std::stoi(ini["settings"]["silencerModel"]);
+        silencer = std::stoi(ini["saves"]["silencer"]);
+
+        sprintf_s(msg, "Config ~g~found~w~:~n~~n~"
+                       "Keybind: ~b~%d~w~~n~"
+                       "Cheat code: ~b~%s~w~~n~"
+                       "BoneID: ~b~%d~w~~n~"
+                       "xyzOffset: ~b~%f %f %f~w~~n~"
+                       "IFP: ~b~%s~w~~n~"
+                       "Anim: ~b~%s~w~~n~"
+                       "AD: ~b~%d~w~~n~"
+                       "AS: ~b~%d~w~~n~"
+                       "M: ~b~%d~w~~n~"
+                       "Silencer: ~b~%d~w~",
+            keyBind, cheatCode.c_str(), boneId, xOffset, yOffset, zOffset, ifpName.c_str(), animName.c_str(), animDuration, animSpot, silencerModel, silencer);
+        CHud::SetHelpMessage(msg, true, false, false);
+    } catch (const std::exception& e) {
+        sprintf_s(msg, "~r~ERR!~w~~n~~n~"
+                       "Keybind: ~b~%d~w~~n~"
+                       "Cheat code: ~b~%s~w~~n~"
+                       "BoneID: ~b~%d~w~~n~"
+                       "xyzOffset: ~b~%f %f %f~w~~n~"
+                       "ifp: ~b~%s~w~~n~"
+                       "anim: ~b~%s~w~~n~"
+                       "AD: ~b~%d~w~~n~"
+                       "AS: ~b~%d~w~~n~"
+                       "M: ~b~%d~w~~n~"
+                       "Silencer: ~b~%d~w~",
+            keyBind, cheatCode.c_str(), boneId, xOffset, yOffset, zOffset, ifpName.c_str(), animName.c_str(), animDuration, animSpot, silencerModel, silencer);
+        CHud::SetHelpMessage(msg, true, false, false);
+    }
+    std::reverse(cheatCode.begin(), cheatCode.end());
+}
+
 struct Main
 {
-    bool silencer;
-    unsigned int key;
-
-    mINI::INIFile file{ PLUGIN_PATH("config.ini") };
-    mINI::INIStructure ini;
-
-    std::string cheatCode;
-
-    void updateConfigs() {
-        static char msg[1024];
-        if (!file.read(ini)) {
-            ini["settings"]["key"] = "192";
-            ini["settings"]["cheat"] = "DSP";
-            ini["saved"]["silencer"] = "1";
-            file.generate(ini);
-        }
-        try {
-            file.read(ini);
-            key = std::stoul(ini["settings"]["key"], nullptr, 0);
-            silencer = std::stoi(ini["saved"]["silencer"]);
-            cheatCode = ini["settings"]["cheat"];
-            sprintf_s(msg, "~g~Refreshed DetachableSilencer!~w~~n~~n~Cheat is: %s~n~Key is: %d~n~Silencer: %d", cheatCode.c_str(), key, silencer);
-            std::reverse(cheatCode.begin(), cheatCode.end());
-            CHud::SetHelpMessage(msg, true, false, false);
-        }
-        catch(const std::exception& e){
-            sprintf_s(msg, "~r~Error when reading the config file:~n~~w~%s", e.what());
-            std::reverse(cheatCode.begin(), cheatCode.end());
-            CHud::SetHelpMessage(msg, true, true, false);
-        }
-    }
-
-    void saveToFile() {
-        ini["saved"]["silencer"] = std::to_string(silencer);
-        file.write(ini);
-    }
+    size_t m_frame = 0; // render frame counter
 
     Main()
     {
         // register event callbacks
-        Events::gameProcessEvent += [] { gInstance.OnGameProcess(); };
-
-        updateConfigs();
+        Events::gameProcessEvent += []{ gInstance.OnGameProcess(); };
+        readConfig();
     }
 
-    bool wasPressed = false;
+    bool wasKeyPressed = false;
     bool wasAlive = true;
 
+    unsigned int lastWeapAmmo = 0;
+    bool hasSavedAmmo = false;
+    unsigned int lastWeapClip;
     eWeaponType lastWeapType;
 
-    unsigned int ammoInGun = 0;
+    CObject* silencerObject = nullptr;
+    bool silencerState = false;
+
+    void updateSave() {
+        ini["saves"]["silencer"] = std::to_string(silencer);
+        file.write(ini);
+    }
+
+    bool isPistol(eWeaponType weapType) {
+        return weapType == WEAPONTYPE_PISTOL || weapType == WEAPONTYPE_PISTOL_SILENCED;
+    }
+
+    unsigned int animDuration = 1500;
+    unsigned int animTime = 0;
+    unsigned int animEnd = 0;
 
     void OnGameProcess()
     {
-        if (strncmp(CCheat::m_CheatString, cheatCode.c_str(), cheatCode.length()) == 0) {
-            CCheat::m_CheatString[0] = '\0';
-            updateConfigs();
-        }
         CPlayerPed* player = FindPlayerPed();
         if (!player) return;
 
+        if (!plugin::Command<0x04EE>(ifpName.c_str())) {
+            plugin::Command<0x04ED>(ifpName.c_str());
+            return;
+        }
+
+        if (strncmp(CCheat::m_CheatString, cheatCode.c_str(), cheatCode.length()) == 0) {
+            CCheat::m_CheatString[0] = '\0';
+            readConfig();
+        }
+
+        CWeapon& currentWeaponRef = player->m_aWeapons[player->m_nSelectedWepSlot];
+        CWeapon currentWeapon = currentWeaponRef;
+        eWeaponType currentWeapType = currentWeapon.m_eWeaponType;
+        unsigned int currentWeapAmmo = currentWeaponRef.m_nAmmoTotal;
+
+        if (isPistol(currentWeapType)) {
+            if (lastWeapType != currentWeapType) {
+                if (currentWeapType == WEAPONTYPE_PISTOL_SILENCED) {
+                    silencer = 1;
+                    updateSave();
+                }
+                if (currentWeapAmmo < lastWeapAmmo) {
+                    currentWeaponRef.m_nAmmoTotal = lastWeapAmmo + currentWeapAmmo;
+                    currentWeaponRef.m_nAmmoInClip = lastWeapClip;
+                } else if(hasSavedAmmo) {
+                    currentWeaponRef.m_nAmmoTotal = lastWeapAmmo;
+                    currentWeaponRef.m_nAmmoInClip = lastWeapClip;
+                }
+            } else {
+                lastWeapAmmo = currentWeapAmmo;
+                lastWeapClip = currentWeaponRef.m_nAmmoInClip;
+                hasSavedAmmo = true;
+            }
+        }
+        lastWeapType = currentWeapType;
+
         bool alive = player->IsAlive();
         if (!alive && wasAlive) {
+            hasSavedAmmo = false;
+            lastWeapType = WEAPONTYPE_UNARMED;
+            lastWeapAmmo = 0;
+            lastWeapClip = 0;
             silencer = 0;
-            saveToFile();
+            updateSave();
         }
         wasAlive = alive;
 
-        unsigned char selectedSlot = player->m_nSelectedWepSlot;
-        CWeapon currentWeapon = player->m_aWeapons[selectedSlot];
-        eWeaponType weaponType = currentWeapon.m_eWeaponType;
-        bool silencerSupport = weaponType == WEAPONTYPE_PISTOL || weaponType == WEAPONTYPE_PISTOL_SILENCED;
+        unsigned int currentTime = CTimer::m_snTimeInMilliseconds;
+        bool giveWeapon = animTime != 0 && animTime < currentTime;
+        bool pressed = KeyPressed(keyBind);
+        if (((pressed && !wasKeyPressed && animTime == 0) || giveWeapon) && isPistol(currentWeapType) && silencer) {
+            if (silencerObject == nullptr) {
+                if (!CStreaming::HasModelLoaded(silencerModel)) {
+                    CStreaming::RequestModel(silencerModel, false);
+                    CStreaming::LoadAllRequestedModels(false);
+                }
+                silencerObject = new CObject(silencerModel, true);
+                CWorld::Add(silencerObject);
+            }
 
-        eWeaponType silPist = WEAPONTYPE_PISTOL_SILENCED;
-        unsigned char playerWeapSlot = player->GetWeaponSlot(silPist);
-        CWeapon& playerWeap = player->m_aWeapons[playerWeapSlot];
-        eWeaponType currentWeapType = playerWeap.m_eWeaponType;
-
-        if (silencerSupport) {
-            if (ammoInGun > playerWeap.m_nAmmoTotal && currentWeapType != lastWeapType) playerWeap.m_nAmmoTotal = ammoInGun + playerWeap.m_nAmmoTotal;
-            ammoInGun = playerWeap.m_nAmmoTotal;
-            lastWeapType = currentWeapType;
-
-            bool pressed = KeyPressed(key);
-            if (pressed && !wasPressed && silencer) {
-                eWeaponType nextWeap = weaponType == WEAPONTYPE_PISTOL ? WEAPONTYPE_PISTOL_SILENCED : WEAPONTYPE_PISTOL;
+            if (giveWeapon) {
+                eWeaponType nextWeap = currentWeapType == WEAPONTYPE_PISTOL ? WEAPONTYPE_PISTOL_SILENCED : WEAPONTYPE_PISTOL;
                 unsigned int nextWeapModel = CWeaponInfo::GetWeaponInfo(nextWeap)->m_nModelId;
                 if (!CStreaming::HasModelLoaded(nextWeapModel)) {
                     CStreaming::RequestModel(nextWeapModel, false);
                     CStreaming::LoadAllRequestedModels(false);
                 }
-                plugin::Command<0x0605>(player, "bomber", "ped", 4.0f, 0, 0, 0, 0, -1);
-                player->GiveWeapon(nextWeap, currentWeapon.m_nAmmoTotal, false);
+                lastWeapAmmo = currentWeapon.m_nAmmoTotal;
+                lastWeapClip = currentWeapon.m_nAmmoInClip;
 
-                CWeapon& givenWeapon = player->m_aWeapons[selectedSlot];
-                givenWeapon.m_nAmmoInClip = currentWeapon.m_nAmmoInClip;
+                player->GiveWeapon(nextWeap, lastWeapAmmo, false);
+                player->m_aWeapons[player->GetWeaponSlot(nextWeap)].m_nAmmoInClip = lastWeapClip;
+
+                CWeapon givenWeapon = player->m_aWeapons[player->GetWeaponSlot(nextWeap)];
+
+                animTime = 0;
+                
+                silencerState = givenWeapon.m_eWeaponType == WEAPONTYPE_PISTOL_SILENCED ? false : true;
+            } else {
+                plugin::Command<COMMAND_TASK_PLAY_ANIM_SECONDARY>(player, animName.c_str(), ifpName.c_str(), 4.0f, 0, 0, 0, 0, 0, -1);
+                animTime = currentTime + animSpot;
+                animEnd = animTime + animDuration;
+
+                silencerState = currentWeapType == WEAPONTYPE_PISTOL_SILENCED ? false : true;
             }
-            wasPressed = pressed;
+        }
+        wasKeyPressed = pressed;
+
+        if (animEnd!= 0 && animEnd < currentTime) {
+            silencerState = false;
         }
 
-        if(!silencer && currentWeapType == silPist) {
-            silencer = 1;
-            saveToFile();
+        if (silencerObject != nullptr) {
+            if (silencerState) {
+                CWorld::Remove(silencerObject);
+                RpHAnimHierarchy* animHierarchy = GetAnimHierarchyFromSkinClump(player->m_pRwClump);
+                if (animHierarchy) {
+                    unsigned int boneIndex = RpHAnimIDGetIndex(animHierarchy, boneId);
+                    RwMatrix* boneMatrix = &RpHAnimHierarchyGetMatrixArray(animHierarchy)[boneIndex];
+                    CMatrix* silencerMatrix = silencerObject->m_matrix;
+                    silencerMatrix->at = boneMatrix->right;
+                    silencerMatrix->pos = boneMatrix->pos + (boneMatrix->right * xOffset) + (boneMatrix->up * yOffset) + (boneMatrix->at * zOffset);
+                    silencerMatrix->up = boneMatrix->up;
+                    silencerMatrix->right = (CVector(-boneMatrix->at.x, -boneMatrix->at.y, -boneMatrix->at.z));
+                    silencerObject->UpdateRwMatrix();
+                    silencerObject->UpdateRwFrame();
+                };
+                CWorld::Add(silencerObject);
+            }
+            else {
+                CWorld::Remove(silencerObject);
+                delete silencerObject;
+                silencerObject = nullptr;
+            }
         }
     }
 } gInstance;
